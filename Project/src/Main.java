4508c8edcb1567252c385f9574189ae2e7c6b882
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-05-13 23:28:51 +0900

    Project
    좌석 생성 및 번호매김까지 FIgma

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -4,19 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int roomNum = 0;
+        int row = 0, col =0, seatNum=0;
         System.out.print("행을 입력해주세요: ");
-        int row = sc.nextInt();
+        int rowCount = sc.nextInt();
         System.out.print("열을 입력해주세요: ");
-        int col = sc.nextInt();
+        int colCount = sc.nextInt();
         System.out.print("빈 열을 입력하세요(없으면 0): ");
         int X = sc.nextInt();
 
-        String[][][] seat;
-        seat = new String[row][col][roomNum];
-        System.out.print(seat);
+        String[][] seat;
+        seat = new String[row][col];
 
-        for (int i = 0; i < col; i++) {
+        for (int i = 0; i <= col; i++) {
+            for (row = 0; row == 0; row++) {
+            }
         }
 
 

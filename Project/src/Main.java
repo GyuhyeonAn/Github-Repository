c9c2ba7155dba0515021996ced61be5bb6c1b644
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-05-19 00:07:55 +0900

    5.18

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,22 +11,22 @@ public class Main {
         System.out.print("열을 입력해주세요: ");
         int colCount = sc.nextInt();
         System.out.print("빈 열을 입력하세요(없으면 0): ");
-        int X = sc.nextInt();
+        int none = sc.nextInt();
 
         int[][] seat;
         seat = new int[colCount][rowCount];
 
-        for (int i = 0; i <= X; i++) {
-            if (X != 0) {
-                seat[i][X] = 0;
+        for (int i = 0; i <= none; i++) {
+            if (none != 0) {
+                seat[i][none] = 0;
                 i++;
             } else break;
         }
 
         for (col = 0; col <= colCount; col++) {
             for (row = 0; row <= rowCount; row++) {
-                if (row != X && col <= colCount) {
-                    seat[col][row] = seatNum++;
+                if (row != none && col <= colCount) {
+                    seat[col][row] = seatNum;
                     row++;
                     seatNum++;
                 }
@@ -34,18 +35,9 @@ public class Main {
                     row = 0;
                     seatNum = 0;
                 }
-                for (int i = 0; i <= colCount; i++) {
-                    int j = 0;
-                    if (j <= rowCount) {
-                        System.out.print((i+1) + "열 " + seat[i][j] + "번 자리 | ");
-                        j++;
-                    }
-                    else {
-                        System.out.println("");
-                        j = 0;
-                        i++;
-                    }
-                }
+
+            System.out.print(Arrays.deepToString(seat));
+
             }
         }
     }

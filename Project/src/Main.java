884cc391b879f177c4d59d8005b8892df5db3af2
GyuhyeonAn <gyuhyeon0035@naver.com>
2025-05-21 00:07:06 +0900

    05.21
    계획서 수정

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -24,6 +24,7 @@ public class Main {
                 }
             }
         }
+        System.out.println("----------현재 좌석-----------");
         for (int col = 0; col < colMax; col++) {
             for (int row = 0; row < rowMax; row++) {
                 System.out.print(seat[col][row]);

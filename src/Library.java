f3afd94f2cbc97a47e7297e6a5d6754d36e17693
jjangjay <jjangjay1@gmail.com>
2025-11-26 14:38:08 +0900

    issue-04

--- src/Library.java
+++ src/Library.java
@@ -9,4 +9,20 @@ public class Library {
 
     public record Book(String title, String author, String KDC) {}
     private static final List<Book> books = new ArrayList<>();
+
+    public static List<Book> searchBook(String query, int searchStandard) {
+        List<Book> searchResults = new ArrayList<>();
+        for (Book book : books) {
+            if (searchStandard == 1 && book.title.contains(query)) {
+                searchResults.add(book);
+            } else if (searchStandard == 2 && book.author.contains(query)) {
+                searchResults.add(book);
+            } else if (searchStandard == 3 && book.KDC.contains(query)) {
+                searchResults.add(book);
+            }
+        }
+
+        return searchResults;
+    }
+
 }
